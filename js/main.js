ac6b684d926c54d166756bb4f6cb07fc26600e25
gareththomasnz <gareththomasnz@yahoo.com>
2016-08-13 10:05:40 +1200

    fifth commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,49 @@
+//var obj = {};
+//obj.prop1 = 3;
+//console.log(obj);
+//
+//function log(arg){
+//        console.log(arg);
+//        }        
+//console.dir(log);
+//
+//var ary = [];
+//ary.prop1 = 3;
+//console.dir(ary);
+
+//var obj = {};
+//obj.name = 'Joe';
+//
+//for(var i = 0; i < 5; i++){
+//        obj['prop' + i] = i;
+//}
+//console.log(obj);
+
+//function fakeAjax(cb){
+//       setTimeout(function(){
+//        cb('ajax data');
+//        }, 1000);
+//}
+//
+//console.log('start');
+//fakeAjax(function(data){
+//        console.log('ajax returned: ' + data);
+//        });
+//console.log('end');
+
+function fakeAjax(cb){
+       setTimeout(function(){
+        cb('ajax data');
+        }, 1000);
+}
+
+console.log('start');
+        fakeAjax(processData);
+console.log('end');
+
+function processData(data){
+        console.log('I have processed the data: ' + data);
+}
+
+
 
